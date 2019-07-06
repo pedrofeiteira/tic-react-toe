691ce766f056e25266489a58edca1da53c3ab60e
Pedro Feiteira <pedro.feiteira@gmail.com>
2019-07-06 08:22:21 +0200

    Updates

---
+++ precache-manifest.aecb2368261715a0f8d25a92628ec268.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c415f4963beb0d4c08424044c31400a8",
+    "url": "/tic-react-toe/index.html"
+  },
+  {
+    "revision": "29cac79b0255b4a98f9a",
+    "url": "/tic-react-toe/static/css/main.c587f7fb.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/tic-react-toe/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "29cac79b0255b4a98f9a",
+    "url": "/tic-react-toe/static/js/main.19a80a43.chunk.js"
+  },
+  {
+    "revision": "5c2de70b98bbb267d2f0",
+    "url": "/tic-react-toe/static/js/runtime~main.c5a0affd.js"
+  }
+]);
